ee518888e8fd97ad87790c52cae7a0e924db6d53
unknown <satriabaskara91@gmail.com>
2024-06-09 00:31:41 +0700

    first commit

---
+++ app/src/main/java/com/example/projekpam/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.projekpam;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ViewPager viewPager;
+    private int[] layouts = {
+            R.layout.onboarding_page1,
+            R.layout.onboarding_page2,
+            R.layout.onboarding_page3
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        viewPager = findViewById(R.id.viewPager);
+        OnboardingPagerAdapter adapter = new OnboardingPagerAdapter(this, layouts);
+        viewPager.setAdapter(adapter);
+    }
+}
