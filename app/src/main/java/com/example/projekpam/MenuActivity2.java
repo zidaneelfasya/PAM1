5b423f4732fdfc323aca0a34baadb8bb6a7f43c7
Satriabaskara29 <138179929+Satriabaskara29@users.noreply.github.com>
2024-06-11 23:00:18 +0700

    add login, onboarding, and home page

---
+++ app/src/main/java/com/example/projekpam/MenuActivity2.java
@@ -0,0 +1,33 @@
+package com.example.projekpam;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MenuActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.recipe2);
+
+
+        Button buttonExitSmoothie = findViewById(R.id.button_exit_smoothie);
+
+        // Set onClick listeners for each button
+        View.OnClickListener exitListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Redirect to BerandaActivity
+                Intent intent = new Intent(MenuActivity2.this, MenuutamaActivity.class);
+                startActivity(intent);
+                finish(); // Close the current activity
+            }
+        };
+
+
+        buttonExitSmoothie.setOnClickListener(exitListener);
+    }
+}
