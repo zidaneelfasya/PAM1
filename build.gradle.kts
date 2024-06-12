461c4f654422f0be0892ca80c3486c49978705b3
zidaneelfasya <111755264+zidaneelfasya@users.noreply.github.com>
2024-06-12 19:18:13 +0700

    update google auth

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,10 @@ buildscript {
     dependencies {
         classpath("com.google.gms:google-services:4.4.2")
     }
+    repositories {
+        google() // Repositori Google
+        mavenCentral()
+    }
 }
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
